e24a28aa767ab7b33ce8a1054322ba14addf680f
Artem <akirillov@itps-russia.ru>
2023-02-14 00:52:30 +0500

    Добавление функции title_text и докстрингов к двум функциям

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def upper_text(str_:str):
+    """Делает все буквы заглавными"""
     return str_.upper()
+
+def title_text(str_:str):
+    """Делает первые буквы слов заглавными"""
+    return str_.title()
